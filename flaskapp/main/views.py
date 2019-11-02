0aeb4da103411b36cea4d3fb26b4014b93ef803a
GreatScot <greatscot010@gmail.com>
2019-11-02 18:15:06 +0000

    format and sort arrival times

--- flaskapp/main/views.py
+++ flaskapp/main/views.py
@@ -1,10 +1,17 @@
 from . import main_blueprint
 from flask import render_template
 from .api_calls import TfLAPICalls
-
+from datetime import datetime
+from dateutil.parser import isoparse
 
 @main_blueprint.route("/")
 def index():
     api = TfLAPICalls()
     response = api.getStopLiveArrivals()
-    return render_template("main/home.html", response = response)
+    for item in response:
+        now = datetime.today()
+        expectedDatetime = datetime.strptime(item['expectedArrival'], '%Y-%m-%dT%H:%M:%SZ')
+        item['expectedArrival'] = round((expectedDatetime-now).seconds/60)
+        item['expectedArrivalString'] = str(item['expectedArrival'])+'m'
+    sortedR = sorted(response, key=lambda item:item['expectedArrival'])
+    return render_template("main/home.html", response = sortedR)

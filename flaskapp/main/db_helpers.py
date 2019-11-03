f0136b2f693c6f4b0bbcd021cfefec0febf1c0c2
GreatScot <greatscot010@gmail.com>
2019-11-03 14:24:02 +0000

    add station name to history

--- flaskapp/main/db_helpers.py
+++ flaskapp/main/db_helpers.py
@@ -4,7 +4,9 @@ from datetime import datetime
 
 def add_to_db(data):
     request_time = datetime.strptime(data[0]['timing']['sent'], '%Y-%m-%dT%H:%M:%SZ')
-    req = Request(request_time = request_time)
+    station_name = data[0]['stationName']
+    naptan_id = data[0]['naptanId']
+    req = Request(request_time = request_time, station_name = station_name, naptan_id = naptan_id)
     for item in data:
         exp_arr_time = datetime.strptime(item['expectedArrival'], '%Y-%m-%dT%H:%M:%SZ')
         exp_arrival = ExpectedArrival(expected_arrival = exp_arr_time, line_name = item['lineName'], destination_name = item['destinationName'], request_id=req.id)
@@ -14,7 +16,7 @@ def add_to_db(data):
     db.session.commit()
 
 def get_history():
-    query = Request.query.options(db.joinedload('expected_arrivals'))
+    query = Request.query.order_by(Request.request_time.desc()).options(db.joinedload('expected_arrivals'))
     requests = []
     for request in query:
         expected_arrivals = []
@@ -29,6 +31,8 @@ def get_history():
             })
         arr_sorted = sorted(expected_arrivals, key=lambda item:item['expected_arrival_number'])
         requests.append({
+            'station_name':request.station_name,
+            'naptan_id':request.naptan_id,
             'request_date':request.request_time.strftime("%d %b %Y"),
             'request_time':request.request_time.strftime("%H:%M"),
             'expected_arrivals':arr_sorted

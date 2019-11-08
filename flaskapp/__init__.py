7a239bb47648b035a261ea102aebc0e04d8d3ac6
GreatScot <greatscot010@gmail.com>
2019-11-08 17:20:28 +0000

    add react app and api blueprint

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -11,9 +11,11 @@ def create_app(config_name):
     db.init_app(app)
 
     from .main import main_blueprint
+    from .api import api_blueprint
     from .error import error_blueprint
 
     app.register_blueprint(main_blueprint)
     app.register_blueprint(error_blueprint)
+    app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app

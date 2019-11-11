7eb33e768a122bf3be343d25ae9294435281d841
GreatScot <greatscot010@gmail.com>
2019-11-11 13:23:20 +0000

    add simple router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import LiveArrivals from './components/LiveArrivals'
+import History from './components/History'
 
 const App = () => {
   const data = fetch('http://localhost:8080/api/data')
     .then(response=>response.json())
     .then(json=>console.log('👌',json))
   return (
-    <div>
-      React App
-    </div>
+    <Router>
+      <Navbar/>
+      <div>
+        React App
+      </div>
+      <Route path="/" exact component={LiveArrivals} />
+      <Route path="/history" component={History} />
+    </Router>
   );
 }
 
---
+++ client/src/components/History.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const History = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default History
